86710235ac04bbc1bdbf411f6dea9af4a0aaab8e
Miguel Contreras <migueldevelop@gmail.com>
2021-06-11 01:47:13 -0400

    First commit

---
+++ src/test/java/io/leantech/employeemanager/EmployeeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.leantech.employeemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
